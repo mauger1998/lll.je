3ac8a2caea9b937a30e2215644ad8307060827cd
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-02-06 21:10:21 +0000

    Add welcome message

--- our-previous-work/work.js
+++ our-previous-work/work.js
@@ -14,4 +14,23 @@ document.addEventListener("click", (e) => {
     })
 })
 
+// CMS
 
+/////////////////// One Small Heading
+
+let PROJECT_ID = "c0i9dxkx";
+let DATASET = "production";
+let QUERY = encodeURIComponent('*[_type == "welcome-message"]');
+
+let URL = `https://c0i9dxkx.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22welcome-message%22%5D`;
+
+fetch(URL)
+        .then((res) => res.json())
+        .then(({ result }) => {
+            let welcomeMessage = document.querySelector(".welcome-right p")
+            result.forEach((result, index) => {
+              welcomeMessage.textContent = result.text
+            });
+            
+        })
+////////////////////////////
